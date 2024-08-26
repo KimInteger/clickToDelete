aac95c7c23bff7e4d2a32ff44dd305a139c57c61
김정수 <kjs20151240@gmail.com>
2024-08-26 17:20:44 +0900

    edit | squrecreate.ts
    랜덤한 위치에서 생성되는 기능 추가

--- src/lib/squrecreate.ts
+++ src/lib/squrecreate.ts
@@ -9,27 +9,38 @@ export class SqureElements {
 
   constructor(root: HTMLDivElement, squreColor: string[]) {
     this.element = document.createElement('div');
-    this.colorIndex = pullRandomInt(0, 4);
+    this.colorIndex = pullRandomInt(0, squreColor.length - 1);
     this.squreBackground = squreColor[this.colorIndex];
     this.count = countToColor(this.squreBackground);
     this.initialize(root);
   }
 
   initialize(root: HTMLDivElement): void {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const randomLeft = pullRandomInt(0, windowWidth - 100);
+    const randomTop = pullRandomInt(0, windowHeight - 100);
+
+    this.element.style.position = 'absolute';
+    this.element.style.left = `${randomLeft}px`;
+    this.element.style.top = `${randomTop}px`;
     this.element.style.width = '100px';
     this.element.style.height = '100px';
     this.element.style.backgroundColor = this.squreBackground;
-    this.element.addEventListener('click', this.decreaseCount);
+    this.element.addEventListener('click', () => {
+      this.decreaseCount;
+    });
     this.element.textContent = `${this.count}`;
     root.appendChild(this.element);
   }
 
-  private decreaseCount(): void {
+  private decreaseCount = (): void => {
     if (this.count > 0) {
       this.count -= 1;
       this.element.textContent = `${this.count}`;
     } else if (this.count <= 0) {
       this.element.remove();
     }
-  }
+  };
 }
