708af6e08a31cd430f6d7d638061c758982640f5
김정수 <kjs20151240@gmail.com>
2024-08-26 17:31:38 +0900

    chore | webpack.config.js
    webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const entryPoint = './src/lib/squreCreateBtn.ts';
+
+module.exports = {
+  entry: entryPoint,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.css', '.js', '.jsx', '.ts', '.tsx'],
+  },
+};
