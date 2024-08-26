71521be672f03c82ffb007b405c754670d5df9cf
김정수 <kjs20151240@gmail.com>
2024-08-26 17:55:24 +0900

    edit | responseSquare.ts
    btn => interval로 변경

--- src/lib/responseSquare.ts
+++ src/lib/responseSquare.ts
@@ -7,17 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
   ) as HTMLDivElement;
 
   if (root) {
-    // 버튼 생성 및 텍스트 추가
-    const squareCreateBtn: HTMLButtonElement = document.createElement('button');
-    squareCreateBtn.textContent = '나와라 상자!';
-
-    // 버튼을 root에 추가
-    root.appendChild(squareCreateBtn);
-
-    // 버튼 클릭 시 네모 생성
-    squareCreateBtn.addEventListener('click', () => {
-      const randomSqure = new SqureElements(root, squreColor);
-    });
+    const createSquare = setInterval(() => {
+      const square = new SqureElements(root, squreColor);
+    }, 1000);
   } else {
     console.error('root가 없단다 친구야.');
   }
